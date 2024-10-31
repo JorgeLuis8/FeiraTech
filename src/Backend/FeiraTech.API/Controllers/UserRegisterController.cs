40f0ec23b70565fd39a96797bdc9e61fe7866f45
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-10-31 16:30:54 -0300

    Criação de arquivos de request e response e definição da controller.

---
+++ src/Backend/FeiraTech.API/Controllers/UserRegisterController.cs
@@ -0,0 +1,21 @@
+﻿using FeiraTech.Communication.Response;
+using FeiraTech.Communication.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FeiraTech.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+
+    [ProducesResponseType(typeof(ResponseRegisterUserJson), StatusCodes.Status201Created)]
+    public class UserRegisterController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Register (RequestRegisterUserJson request)
+        {
+            return Created()
+;        }
+
+    }
+}
---
+++ src/Shared/FeiraTech.Communication/Requests/RequestRegisterUserJson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeiraTech.Communication.Requests
+{
+    public class RequestRegisterUserJson
+    {
+        string Name { get; set; } = string.Empty;
+        string Email { get; set; } = string.Empty;
+        string Password { get; set; } = string.Empty;
+        string CPF { get; set; } = string.Empty;
+        string Phone { get; set; } = string.Empty;
+
+    }
+}
