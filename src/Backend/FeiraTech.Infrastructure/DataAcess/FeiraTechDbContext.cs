c3a9838266df1dc21d54d14a8aed983dc78cde51
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-10-31 21:26:52 -0300

    criação do repositorio

---
+++ src/Backend/FeiraTech.Infrastructure/DataAcess/FeiraTechDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeiraTech.Infrastructure.DataAcess
+{
+    public class FeiraTechDbContext : DbContext
+    {
+        public FeiraTechDbContext(DbContextOptions<FeiraTechDbContext> options) : base(options)
+        {
+        }
+        public DbSet<FeiraTech.Domain.Entity.User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(FeiraTechDbContext).Assembly);
+        }
+
+    }
+}
---
+++ src/Backend/FeiraTech.Infrastructure/DataAcess/Repositories/UserRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeiraTech.Infrastructure.DataAcess.Repositories
+{
+    public class UserRepository
+    {
+        private readonly FeiraTechDbContext _context;
+
+        public UserRepository(FeiraTechDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Add(FeiraTech.Domain.Entity.User user)
+        {
+            await _context.Users.AddAsync(user);
+        }
+
+        public async Task<bool> EmailIsExists(string email)
+        {
+            return await _context.Users.AnyAsync(user => user.Email.Equals(email) && user.Active);
+
+        }
+    }
+}
