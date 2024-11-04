a1ddb46b8e1901158720c7bbef4565f41ddbb09f
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-11-04 17:35:59 -0300

    Criando migrações de forma automatizada das tabelas

---
+++ src/Backend/FeiraTech.Infrastructure/Migrations/Versions/Version0000001.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+
+namespace FeiraTech.Infrastructure.Migrations.Versions
+{
+    //Criando a primeira versão do banco de dados usando o FluentMigrator
+
+    [Migration(DatabaseVersions.TABLE_USERS, "Created table the user save information")]
+    public class Version0000001 : VersionBase
+    {
+        public override void Up()
+        {
+            CreateTable("Users")
+                .WithColumn("Name").AsString(100).NotNullable()
+                .WithColumn("Email").AsString(100).NotNullable()
+                .WithColumn("Password").AsString(2000).NotNullable()
+                .WithColumn("CPF").AsString(11).NotNullable()
+                .WithColumn("Phone").AsString(11).NotNullable();
+
+
+
+        }
+    }
+}
